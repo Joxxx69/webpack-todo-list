33e2f5c404569cb23c915605b3bd1f49cc7cd096
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-04-06 23:28:45 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {saludar} from './js/componentes'
+import '../src/style.css'
+
+// const saludar = require('./js/componentes');
+
+const nombre = 'Santiago Gualotuna';
+
+saludar(nombre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
